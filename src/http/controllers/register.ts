434a6e0fc31a60043388bdc8705686bbd8bb3407
Paulo301 <paulovictor1918@gmail.com>
2023-08-31 23:41:35 -0300

    Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,11 +1,9 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-export async function register(
-  request: FastifyRequest,
-  response: FastifyReply,
-) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
@@ -14,13 +12,25 @@ export async function register(
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
-  return response.status(201).send()
+  return reply.status(201).send()
 }

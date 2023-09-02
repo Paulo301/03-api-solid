cd5c3735267e8bbcdff4bc19a10ec431122c3262
Paulo301 <paulovictor1918@gmail.com>
2023-09-02 12:41:34 -0300

    Testes e controller de autenticação

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -25,7 +25,7 @@ export class AuthenticateUseCase {
       throw new InvalidCredentialsError()
     }
 
-    const doesPasswordsMatches = compare(password, user.password_hash)
+    const doesPasswordsMatches = await compare(password, user.password_hash)
 
     if (!doesPasswordsMatches) {
       throw new InvalidCredentialsError()
